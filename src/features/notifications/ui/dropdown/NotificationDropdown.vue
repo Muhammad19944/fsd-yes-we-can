b93ada097fa16d9e23b664669c90155f9202ee46
Muhammad <31342676+Muhammad19944@users.noreply.github.com>
2024-09-25 23:31:53 +0500

    test commit

--- src/features/notifications/ui/dropdown/NotificationDropdown.vue
+++ src/features/notifications/ui/dropdown/NotificationDropdown.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+let c = '123213'
+let g = 12333
+</script>
 
 <template>
 	<div class="notification-dropdown">notification-dropdown</div>
